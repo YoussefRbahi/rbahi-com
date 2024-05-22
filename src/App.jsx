8f043c3f9bbc57a8ff7eacee0feb16664ead727d
Youssef Rbahi <youssefrbahi@gmail.com>
2024-05-22 19:37:21 +0300

    Create a mesh gradient background for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,12 @@
+import "./App.css";
 function App() {
-  return <></>;
+  return (
+    <>
+      <div className="snap-y snap-mandatory h-screen overflow-y-auto  overflow-x-hidden text-slate-800 relative">
+        <div className="gradient-bg h-screen overflow-hidden"></div>
+      </div>
+    </>
+  );
 }
 
 export default App;
