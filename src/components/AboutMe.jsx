d0c654fbabd9c6bdf22e86482282faa8eed768e3
Youssef Rbahi <youssefrbahi@gmail.com>
2024-06-04 02:25:13 +0300

    Added dummy content to the About and Projects components

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -18,6 +18,29 @@ export default function AboutMe() {
           <li>CSS</li>
         </ul>
       </div>
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat. Duis aute irure dolor in reprehenderit in voluptate
+      velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat
+      cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id
+      est laborum."
+      <em>
+        <em>
+          e e
+          <em>
+            <em>
+              <em>
+                e
+                <em>
+                  eqfe qewf qwef qwef wqef wqe
+                  <fieldset>wqefqw ef</fieldset>
+                </em>
+              </em>
+            </em>
+          </em>
+        </em>
+      </em>
     </div>
   );
 }
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,1 +1,17 @@
-export default function Projects() {}
+export default function Projects() {
+  return (
+    <div>
+      <p>Fusce et ante luctus, lobortis nisi eget, tincidunt augue.</p>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      <p>
+        Phasellus molestie magna non est bibendum non venenatis nisl tempor.
+      </p>
+      <p>Mauris dapibus risus quis suscipit vulputate.</p>
+      <p>Ut in nulla enim.</p>
+      <p>Vivamus hendrerit arcu sed erat molestie vehicula.</p>
+      <p>Eros diam egestas libero eu vulputate risus.</p>
+      <p>Quisque aliquam dignissim purus in iaculis.</p>
+      <p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
+    </div>
+  );
+}
