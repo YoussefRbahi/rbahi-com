93175b1c93b121470152ece23c0811cdee940afd
Youssef Rbahi <youssefrbahi@gmail.com>
2024-06-11 14:38:05 +0300

    Fix MainPart and style it better

--- src/components/MainPart.jsx
+++ src/components/MainPart.jsx
@@ -10,13 +10,9 @@ export default function MainPart() {
   const sections = [
     { key: "about", title: "About Me", component: <AboutMe /> },
     { key: "projects", title: "Projects", component: <Projects /> },
-    {
-      key: "experience",
-      title: "Experience",
-      component: <div>Experience</div>,
-    },
-    { key: "blog", title: "Blog", component: <div>Blog</div> },
-    { key: "contact", title: "Contact", component: <div>Contact</div> },
+
+    //{ key: "blog", title: "Blog", component: <div>Blog</div> },
+    // { key: "contact", title: "Contact", component: <div>Contact</div> },
   ];
 
   useEffect(() => {
@@ -24,8 +20,8 @@ export default function MainPart() {
   }, []);
 
   return (
-    <div className="relative grid grid-cols-1 md:grid-cols-2 overflow-visible">
-      <div className="flex flex-col justify-between md:h-screen md:sticky top-0 md:py-28 w-2/3">
+    <div className="relative grid grid-cols-1 md:grid-cols-2">
+      <div className="flex flex-col justify-between md:h-screen md:sticky top-0 md:py-24 w-2/3">
         <div>
           <Bio />
           <div className="hidden md:block h-min text-xl mt-8">
@@ -35,21 +31,20 @@ export default function MainPart() {
         <Socials />
       </div>
 
-      <div className="grid text-md md:py-28 gap-20">
-        {sections.map((section) => (
-          <div key={section.key}>
-            <Element
-              name={section.key}
-              id={section.key}
-              className="grid relative"
-            >
-              <div className="sticky top-0 block py-2 bg-opacity-50 md:hidden">
-                <h2>{section.title}</h2>
-              </div>
-              {section.component}
-            </Element>
-            <hr />
-          </div>
+      <div className="grid text-md py-0 md:py-24">
+        {sections.map((section, index) => (
+          <Element
+            key={section.key}
+            name={section.key}
+            id={section.key}
+            className="grid relative"
+          >
+            <div className="sticky top-0 block py-2 bg-opacity-50 md:hidden">
+              <h2>{section.title}</h2>
+            </div>
+            {section.component}
+            {index !== sections.length - 1 && <hr className="my-8" />}
+          </Element>
         ))}
       </div>
     </div>
