406d6d65b584c12d20e8787ef991f1a043a6dd9c
Youssef Rbahi <youssefrbahi@gmail.com>
2024-06-11 14:40:29 +0300

    Create Projects component and add project imgaes

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,69 @@
 export default function Projects() {
+  const projects = [
+    {
+      name: "Jeopardy Clone",
+      description:
+        "A web-based clone of the popular game show Jeopardy, created using only Vanilla JavaScript, HTML, and CSS. This was my first JavaScript project, featuring a simple interface for question selection and responses. Questions and answers are stored as JSON in text files, demonstrating my initial handling of JavaScript for data storage.",
+      image: "/images/jeopardy.png",
+      link: "https://github.com/YoussefRbahi/jeopardy",
+      tags: ["HTML", "CSS", "JavaScript"],
+    },
+    {
+      name: "Hikma Education",
+      description:
+        "A website for Hikma Education, an agency that provides educational services in the Middle East. This was my first project that involved a front-end framework, Vue.js, and a back-end framework, Strapi. I also used Tailwind CSS for styling. The website is RTL-based but supports LTR and features a responsive design for different screen sizes.",
+      image: "/images/hikma-education.png",
+      link: "https://hikmaeducation.netlify.app/",
+
+      tags: ["Vue", "Tailwind CSS", "Strapi"],
+    },
+    {
+      name: "rbahi.com",
+      description:
+        "My portfolio website built from scratch using React and Tailwind CSS. It features a very minimal design heavily inspired by the work of Brittany Chiang and Sean Halpin. I have previously implemented multiple features that make use of REST APIs, including a weather widget, and a contact form, that I ended up scrapping to get a simpler and more streamlined design.",
+      image: "/images/rbahi.png",
+      link: "https://rbahi.com",
+
+      tags: ["React", "Tailwind CSS", "REST API"],
+    },
+  ];
   return (
-    <div>
-      <p>Fusce et ante luctus, lobortis nisi eget, tincidunt augue.</p>
-      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
-      <p>
-        Phasellus molestie magna non est bibendum non venenatis nisl tempor.
-      </p>
-      <p>Mauris dapibus risus quis suscipit vulputate.</p>
-      <p>Ut in nulla enim.</p>
-      <p>Vivamus hendrerit arcu sed erat molestie vehicula.</p>
-      <p>Eros diam egestas libero eu vulputate risus.</p>
-      <p>Quisque aliquam dignissim purus in iaculis.</p>
-      <p>Sed auctor neque eu tellus rhoncus ut eleifend nibh porttitor.</p>
-    </div>
+    <ul className="grid gap-8 text-justify">
+      {projects.map((project, index) => (
+        <li key={index} className="grid gap-4">
+          <div className="flex gap-4">
+            <div className=" w-1/3 rounded-sm border border-highcontrast ">
+              <img
+                src={project.image}
+                alt="project-image"
+                className="rounded-sm"
+              />
+            </div>
+
+            <div className="flex flex-col gap-4">
+              <a href={project.link} className="relative block">
+                <h3 className="text-3xl font-medium text-highcontrast after:content-['↗'] after:text-2xl after:absolute after:ms-1 after:transition-transform hover:after:-translate-y-1 hover:after:translate-x-1">
+                  {project.name}
+                </h3>
+              </a>
+
+              <div className="flex gap-2">
+                {project.tags.map((tag, index) => (
+                  <span
+                    key={index}
+                    className="rounded-full px-2 py-1 text-xs font-semibold text-primary bg-highcontrast"
+                  >
+                    {tag}
+                  </span>
+                ))}
+              </div>
+            </div>
+          </div>
+          <div className="text-sm">
+            <p>{project.description}</p>
+          </div>
+        </li>
+      ))}
+    </ul>
   );
 }
