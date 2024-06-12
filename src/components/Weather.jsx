6d4f8bafbc6c4a31790da0782138c6cb179dbf31
Youssef Rbahi <youssefrbahi@gmail.com>
2024-06-12 19:58:01 +0300

    Make Weather icon responsive

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -5,7 +5,7 @@ function Weather({ userData }) {
         src={userData.current.condition.icon}
         alt={userData.current.condition.text}
         title={userData.current.condition.text}
-        className="inline-block h-8"
+        className="inline-block h-8 md:h-10 lg:h-12"
       />
       <span className=" "> {Math.round(userData.current.temp_c)}° C</span>
     </div>
