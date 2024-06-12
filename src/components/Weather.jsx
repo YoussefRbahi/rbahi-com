a4e243d428dc6fedb11a6938474a1f4f8998bb66
Youssef Rbahi <youssefrbahi@gmail.com>
2024-06-12 18:45:53 +0300

    Round temperature and align icon with text

--- src/components/Weather.jsx
+++ src/components/Weather.jsx
@@ -1,14 +1,14 @@
 function Weather({ userData }) {
   return (
-    <>
+    <div className="flex items-center">
       <img
         src={userData.current.condition.icon}
         alt={userData.current.condition.text}
         title={userData.current.condition.text}
-        className="inline-block"
-      />{" "}
-      {userData.current.temp_c}° C
-    </>
+        className="inline-block h-8"
+      />
+      <span className=" "> {Math.round(userData.current.temp_c)}° C</span>
+    </div>
   );
 }
 export default Weather;
