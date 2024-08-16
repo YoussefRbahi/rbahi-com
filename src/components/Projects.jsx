e26acf41a70ab8bbaf79353a6547738e58a043c7
Youssef Rbahi <72280234+YoussefRbahi@users.noreply.github.com>
2024-08-16 19:53:43 +0300

    Add IRL Chat

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,9 +1,18 @@
 export default function Projects() {
   const projects = [
     {
+      name: "IRL Chat",
+      description:
+        "Real-time chat web app built using react-peer-rooms (see below) and Next.js. It allows you have a chat screen for live events where attendees can send and see messages from everyone in real-time. Connecting is as simple as opening a link or scanning a QR code and entering a nickname.",
+      image: "/images/irlchat.png",
+      github: "https://github.com/youssefrbahi/irl-chat",
+      link: "https://irlchat.netlify.app",
+      tags: ["React", "TypeScript", "Next.js"],
+    },
+    {
       name: "react-peer-rooms",
       description:
-        "A React hooks library for centralized yet serverless P2P communication. It allows you to easily create PeerJS-based WebRTC rooms for data exchange using a host/client(s) model. ",
+        "React hooks library for centralized yet serverless P2P communication. It allows you to easily create PeerJS-based WebRTC rooms for data exchange using a host/client(s) model. ",
       image: "/images/npm.png",
       github: "https://github.com/youssefrbahi/react-peer-rooms",
       link: "https://www.npmjs.com/package/react-peer-rooms",
@@ -12,7 +21,7 @@ export default function Projects() {
     {
       name: "Fontcetera",
       description:
-        "Fontcetera is a Chrome extension developed with React and crxjs that enables users to effortlessly format text with various Unicode fonts and styles. It offers a real-time preview and easy copy-to-clipboard functionality within a simple and intuitive user interface.",
+        "Chrome extension developed with React and crxjs that enables users to effortlessly format text with various Unicode fonts and styles. It offers a real-time preview and easy copy-to-clipboard functionality within a simple and intuitive user interface.",
       image: "/images/fontcetera.png",
       github: "https://github.com/youssefrbahi/fontcetera",
       link: "https://chromewebstore.google.com/detail/fontcetera-unicode-text-f/efpedoejhnkhmjeclomcoamlbmohcaie?hl=en",
@@ -37,7 +46,7 @@ export default function Projects() {
     {
       name: "rbahi.com",
       description:
-        "My portfolio website built from scratch using React and Tailwind CSS. It features a very minimal design heavily inspired by the work of Brittany Chiang and Sean Halpin. The lockscreen is inspired by the iOS lockscreen (and by videogame cutscenes on desktop) and uses ipify and WeatherAPI to get the user's location and weather information.",
+        "Portfolio website built using React and Tailwind CSS. It features a very minimal design heavily inspired by the work of Brittany Chiang and Sean Halpin. The lockscreen is inspired by the iOS lockscreen (and by videogame cutscenes on desktop) and uses ipify and WeatherAPI to get the user's location and weather information.",
       image: "/images/rbahi.png",
       link: "https://rbahi.com",
       github: "https://github.com/YoussefRbahi/rbahi-com",
@@ -72,7 +81,7 @@ export default function Projects() {
                 rel="noopener noreferrer"
                 className="relative block text-highcontrast"
               >
-                <h3 className="text-3xl md:text-2xl font-medium">
+                <h3 className="text-3xl md:text-2xl font-medium text-nowrap">
                   {/* <h3 className="text-3xl md:text-2xl font-medium text-highcontrast after:content-['↗'] after:text-2xl after:absolute after:ms-1 after:transition-transform hover:after:-translate-y-1 hover:after:translate-x-1"> */}
                   {project.name}
                 </h3>
@@ -96,6 +105,8 @@ export default function Projects() {
               {project.link && (
                 <a
                   href={project.github}
+                  rel="noopener noreferrer"
+                  target="_blank"
                   className="relative inline-block font-medium text-highcontrast underline"
                 >
                   Github
